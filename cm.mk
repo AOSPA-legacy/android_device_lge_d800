9c41a43783a5d1d401f858d8d5d005a6412d0917
Michael S Corigliano <michael.s.corigliano@gmail.com>
2015-01-13 00:43:43 -0500

    Adapt for AOSPA

--- cm.mk
+++ cm.mk
@@ -5,22 +5,20 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common PA stuff.
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/pa/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d800/d800.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := d800
-PRODUCT_NAME := cm_d800
+PRODUCT_NAME := pa_d800
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D800
 PRODUCT_MANUFACTURER := lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_att_us/g2:4.4.2/KOT49I.D80020k/D80020k.1393917793:user/release-keys PRIVATE_BUILD_DESC="g2_att_us-user 4.4.2 KOT49I.D80020k D80020k.1393917793 release-keys"
-
-PRODUCT_PACKAGES += Torch
